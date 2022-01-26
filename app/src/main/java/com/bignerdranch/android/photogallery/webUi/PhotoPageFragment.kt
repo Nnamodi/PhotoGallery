8108ba290d030cf9f375e4a3046d59ab0f0d876c
Roland <nnamdiigede11@gmail.com>
2022-01-26 17:21:25 +0100

    All trimmed up

--- app/src/main/java/com/bignerdranch/android/photogallery/webUi/PhotoPageFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/webUi/PhotoPageFragment.kt
@@ -6,14 +6,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.activity.addCallback
+import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.photogallery.R
 import com.bignerdranch.android.photogallery.VisibleFragment
 
 private const val ARG_URI = "photo_page_url"
 
 class PhotoPageFragment : VisibleFragment() {
+    private lateinit var progressBar: ProgressBar
     private lateinit var uri: Uri
     private lateinit var webView: WebView
 
@@ -25,10 +30,34 @@ class PhotoPageFragment : VisibleFragment() {
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_photo_page, container, false)
+        progressBar = view.findViewById(R.id.progress_bar)
+        progressBar.max = 100
         webView = view.findViewById(R.id.web_view)
         webView.settings.javaScriptEnabled = true
         webView.webViewClient = WebViewClient()
         webView.loadUrl(uri.toString())
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                if (newProgress == 100) {
+                    progressBar.visibility = View.GONE
+                } else {
+                    progressBar.visibility = View.VISIBLE
+                    progressBar.progress = newProgress
+                }
+            }
+
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                (activity as AppCompatActivity).supportActionBar?.subtitle = title
+            }
+        }
+        /** Based on a challenge. */
+        activity?.onBackPressedDispatcher?.addCallback(this) {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            } else {
+                requireActivity().finish()
+            }
+        }
         return view
     }
 
